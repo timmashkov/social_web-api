c1ec5d5d772445f31a706fca883a455b81636c75
timmashkov <bodomizer@yandex.ru>
2024-02-19 11:47:00 +0300

    created first schema

---
+++ src/schemas/user.py
@@ -0,0 +1,24 @@
+from typing import Optional
+from uuid import UUID
+
+from pydantic import BaseModel, EmailStr, field_validator
+
+
+class UserIn(BaseModel):
+    first_name: str
+    last_name: str
+    age: int | float = 0
+    city: str
+    email: EmailStr
+    phone_number: str
+    occupation: Optional[str] = None
+
+    @field_validator("phone_number")
+    def check_phone(cls, data):
+        if not data.isdigit() and len(data) != 11:
+            raise ValueError("The phone number should be 11 characters in length and be numbers")
+        return data
+
+
+class UserOut(UserIn):
+    id: UUID
