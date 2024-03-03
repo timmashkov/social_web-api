db5c1b015367e58b3d895098a469ae71cf1dd334
timmashkov <bodomizer@yandex.ru>
2024-03-03 13:00:06 +0300

    added session and abstract repo

---
+++ server_microservice/app_src/infrastructure/database/abs_repository.py
@@ -0,0 +1,43 @@
+from uuid import UUID
+
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select, insert, update, delete
+
+
+class AbstractRepository:
+    def __init__(self, model, session: AsyncSession) -> None:
+        self.model = model
+        self.session = session
+
+    async def get_all(self):
+        stmt = select(self.model).order_by(self.model.id)
+        answer = await self.session.execute(stmt)
+        result = answer.scalars().all()
+        return result
+
+    async def get_by_id(self, model_id: UUID):
+        stmt = select(self.model).where(self.model.id == model_id)
+        answer = await self.session.execute(stmt)
+        result = answer.scalar_one_or_none()
+        return result
+
+    async def create(self, data):
+        stmt = insert(self.model).values(**data.model_dump()).returning(self.model)
+        answer = await self.session.execute(stmt)
+        await self.session.commit()
+        result = answer.mappings().first()
+        return result
+
+    async def update(self, data, model_id: UUID):
+        stmt = update(self.model).where(self.model.id == model_id).values(**data.model_dump()).returning(self.model)
+        answer = await self.session.execute(stmt)
+        await self.session.commit()
+        result = answer.mappings().first()
+        return result
+
+    async def delete(self, model_id: UUID):
+        stmt = delete(self.model).where(self.model.id == model_id).returning(self.model)
+        answer = await self.session.execute(stmt)
+        await self.session.commit()
+        result = answer.mappings().first()
+        return result
---
+++ server_microservice/app_src/infrastructure/database/session.py
@@ -0,0 +1,22 @@
+from typing import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+
+from infrastructure.settings.config import settings
+
+
+class SessionAdaptor:
+    def __init__(self, url: str, echo: bool) -> None:
+        self.engin = create_async_engine(url=url, echo=echo)
+        self.session_factory = async_sessionmaker(bind=self.engin,
+                                                  autoflush=False,
+                                                  autocommit=False,
+                                                  expire_on_commit=False)
+
+    async def session_local(self) -> AsyncGenerator:
+        async with self.session_factory() as session:
+            yield session
+            await session.close()
+
+
+connector = SessionAdaptor(url=settings.db_url, echo=settings.echo)
