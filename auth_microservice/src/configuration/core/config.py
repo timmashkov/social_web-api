03c2424f7113f3fe7aa2465673e6816116c02328
timmashkov <bodomizer@yandex.ru>
2024-03-11 20:09:07 +0300

    some minor changes

--- auth_microservice/src/configuration/core/config.py
+++ auth_microservice/src/configuration/core/config.py
@@ -1,4 +1,5 @@
 import os
+from typing import Literal
 
 from pydantic_settings import BaseSettings
 from dotenv import load_dotenv
@@ -9,6 +10,9 @@ load_dotenv()
 class DevConfig(BaseSettings):
     """Конфиг со всеми чувствительными данными"""
 
+    # apllication mode
+    mode: Literal["DEV", "TEST", "PROD"]
+    # main database
     DB_HOST: str = os.environ.get("DB_HOST")
     DB_PORT: int = os.environ.get("DB_PORT")
     DB_NAME: str = os.environ.get("DB_NAME")
@@ -18,14 +22,24 @@ class DevConfig(BaseSettings):
         f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
     )
     echo: bool = False
-
+    # test database
+    TEST_DB_HOST: str = os.environ.get("TEST_DB_HOST")
+    TEST_DB_PORT: int = os.environ.get("TEST_DB_PORT")
+    TEST_DB_NAME: str = os.environ.get("TEST_DB_NAME")
+    TEST_DB_USER: str = os.environ.get("TEST_DB_USER")
+    TEST_DB_PASS: str = os.environ.get("TEST_DB_PASS")
+    test_db_url: str = (
+        f"postgresql+asyncpg://{TEST_DB_USER}:{TEST_DB_PASS}@{TEST_DB_HOST}:{TEST_DB_PORT}/{TEST_DB_NAME}"
+    )
+    test_echo: bool = False
+    # secrets
     SECRET: str = os.environ.get("SECRET_KEY")
-
+    # redis
     REDIS_HOST: str = os.environ.get("REDIS_HOST")
     REDIS_PORT: str = os.environ.get("REDIS_PORT")
     EXPIRATION: int = 60
     REDIS_URL: str = f"redis://{REDIS_HOST}:{REDIS_PORT}"
-
+    # rabbitmq
     RABBIT_NAME: str = os.environ.get("RABBIT_NAME")
     RABBIT_PASS: str = os.environ.get("RABBIT_PASS")
     RABBIT_HOST: str = os.environ.get("RABBIT_HOST")
--- auth_microservice/src/configuration/core/database.py
+++ auth_microservice/src/configuration/core/database.py
@@ -1,6 +1,7 @@
 from typing import AsyncGenerator
 
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy import NullPool
 
 from configuration.core.config import base_config
 
@@ -21,3 +22,21 @@ class SessionConnector:
 
 
 connector = SessionConnector(base_config.db_url, base_config.echo)
+
+
+class TestSessionConnector:
+    """Тестовый коннектор к сессии алхимии"""
+
+    def __init__(self, url: str, echo: bool) -> None:
+        self.engine = create_async_engine(url=url, echo=echo, poolclass=NullPool)
+        self.session_fabric = async_sessionmaker(
+            bind=self.engine, autoflush=False, autocommit=False, expire_on_commit=False
+        )
+
+    async def scoped_session(self) -> AsyncGenerator:
+        async with self.session_fabric() as session:
+            yield session
+            await session.close()
+
+
+test_connector = TestSessionConnector(base_config.test_db_url, base_config.test_echo)
